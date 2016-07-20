c819e9117c87657df61efdd96f274d2a0d8fb5fa
Omri Gazitt <ogazitt@gmail.com>
2016-07-20 15:03:16 -0700

    Initial Commit

---
+++ ExampleCommandLine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExampleCommandLine
+//
+//  Created by Omri Gazitt on 7/20/16.
+//  Copyright © 2016 Omri Gazitt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
